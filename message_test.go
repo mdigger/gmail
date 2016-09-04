e0af6a6450c669bf6fc153540d5afff9472f3463
Дмитрий Седых <dmitrys@xyzrd.com>
2016-09-05 00:47:13 +0300

    tests

--- message_test.go
+++ message_test.go
@@ -2,6 +2,7 @@ package gmail
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
@@ -13,7 +14,9 @@ func TestMessageText(t *testing.T) {
 			"I am<sedykh@gmail.com>",
 			"I am too <dmitrys@xyzrd.com>",
 			"d3@yandex.ru"},
-		nil)
+		[]string{"Дмитрий Седых <d3@yandex.ru>"},
+		nil,
+	)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -21,16 +24,27 @@ func TestMessageText(t *testing.T) {
 	// err = msg.Body([]byte(`<p>html body</p>`))
 	err = msg.Body(html)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
-	// err = msg.File("test_file.html", html)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
-	// err = msg.File("test_file.txt", text)
-	// if err != nil {
-	// 	t.Fatal(err)
-	// }
+	err = msg.File("test_file.html", html)
+	if err != nil {
+		t.Error(err)
+	}
+	err = msg.File("test_file.txt", text)
+	if err != nil {
+		t.Error(err)
+	}
+	err = msg.File("test_file.txt", nil)
+	if err != nil {
+		t.Error(err)
+	}
+	if msg.Has("test_file.txt") {
+		t.Error("bad delete attachment")
+	}
+	err = msg.File("test_file.bin", bin)
+	if err != nil {
+		t.Error(err)
+	}
 
 	// pretty.Println(msg)
 
@@ -39,6 +53,74 @@ func TestMessageText(t *testing.T) {
 		t.Fatal(err)
 	}
 	print(buf.String())
+}
+
+func TestBadMessage(t *testing.T) {
+	if _, err := NewMessage("", "me", []string{"bad"}, nil, nil); err == nil {
+		t.Error(err)
+	}
+	if _, err := NewMessage("", "me", nil, []string{"bad"}, nil); err == nil {
+		t.Error(err)
+	}
+	if _, err := NewMessage("", "bad", nil, nil, nil); err == nil {
+		t.Error("bad from email")
+	}
+	if _, err := NewMessage("", "me", []string{"d3@ya.ru"}, nil, bin); err == nil {
+		t.Error("bad message text")
+	}
+	if _, err := NewMessage("", "me", nil, nil, nil); err == nil {
+		t.Error("bad recipient")
+	}
+}
+
+func TestBadAttach(t *testing.T) {
+	msg, err := NewMessage("", "", []string{"d3@yandex.ru"}, nil, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := msg.File("", html); err == nil {
+		t.Error("bad file name")
+	}
+	if err := msg.writeTo(ioutil.Discard); err == nil {
+		t.Error("content undefined")
+	}
+	if err := msg.File("~/test/file.name", html); err != nil {
+		t.Error(err)
+	}
+	if err := msg.writeTo(ioutil.Discard); err != nil {
+		t.Error(err)
+	}
+	if err := msg.File("..", text); err == nil {
+		t.Error("bad file name 2")
+	}
+	if err := msg.File("~/test/file.name", text); err != nil {
+		t.Error(err)
+	}
+	if err := msg.File("~/test/file.name", bin); err != nil {
+		t.Error(err)
+	}
+
+	// pretty.Println(msg)
+}
+
+func TestSimpleMessage(t *testing.T) {
+	msg, err := NewMessage(
+		"Subject",
+		"",
+		[]string{"Дмитрий Седых <d3@yandex.ru>"},
+		nil,
+		html,
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := msg.writeTo(ioutil.Discard); err != nil {
+		t.Error(err)
+	}
+	gmailService = nil
+	if msg.Send() == nil {
+		t.Error("not initialized service")
+	}
 }
 
 func TestMessageSend(t *testing.T) {
@@ -50,22 +132,18 @@ func TestMessageSend(t *testing.T) {
 		"",
 		[]string{"Дмитрий Седых <d3@yandex.ru>"},
 		nil,
+		html,
 	)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// msg.Body(text)
-	err = msg.Body(html)
 	if err != nil {
 		t.Fatal(err)
 	}
 	err = msg.File("test_file.html", html)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 	err = msg.File("test_file.txt", text)
 	if err != nil {
-		t.Fatal(err)
+		t.Error(err)
 	}
 
 	// pretty.Println(msg)
@@ -76,11 +154,10 @@ func TestMessageSend(t *testing.T) {
 }
 
 var (
-	text = []byte(`Message body
-
-This is a text text message.
-`)
-	html = []byte(`<html><h2>Message body</h2>
-
-<p>This is a html text message.</p></html>`)
+	text = []byte("Message body\nThis is a text text message.")
+	html = []byte(`<html>
+<h2>Message body</h2>
+<p>This is a html text message.</p>
+</html>`)
+	bin = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 )
